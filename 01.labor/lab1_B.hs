4c6bc8f573ffc20fccae91a7fa13ba8db4b9179a
feketicsheni <feketicsheni@gmail.com>
2026-02-26 11:38:58 +0200

    lab02_ lab1 done

--- 01.labor/lab1_B.hs
+++ 01.labor/lab1_B.hs
@@ -1,3 +1,4 @@
+import System.Win32 (LOCALESIGNATURE(lsCsbDefault))
 -- I. Könyvtárfüggvények használata nélkül, definiáljuk azt a függvényt, amely meghatározza
 
 -- - két szám összegét, különbségét, szorzatát, hányadosát, osztási maradékát,
@@ -57,21 +58,177 @@ min_ a b
 -- - egy másodfokú egyenlet gyökeit,
 -- - hogy két elempár értékei "majdnem" megegyeznek-e: akkor térít vissza True értéket a függvény, ha a két pár ugyanazokat az értékeket tartalmazza függetlenül az elemek sorrendjétől.
 --   Például: $$(6, 7)$$ egyenlő $$(7,6)$$-al, de $$(6, 7)$$ nem egyenlő $$(4, 7)$$-el.
+elempar ep1 ep2 = (a==c && b==d) || (a==d && b==c)
+  where
+    (a,b)=ep1
+    (c,d)=ep2
+
+elempar2 (a,b) (c,d) = (a==c && b==d) || (a==d && b==c)
+
+
+
+
 -- - az n szám faktoriálisát (3 módszer),
+fakt1 0 = 1 
+fakt1 n= n* fakt1 (n-1)
+
+
+
+fakt2 :: (Integral a) => a -> a
+fakt2 n
+  | n < 0     = error "neg. szam"
+  | n == 0    = 1
+  | otherwise = n * fakt2 (n - 1)
+
+
+
+fakt3 :: (Integral a) => a -> a -> a
+fakt3 n res
+  | n < 0     = error "neg. szam"
+  | n == 0    = res
+  | otherwise = fakt3 (n - 1) (res * n)
+
+
+
+
+
+
+
 -- - az x szám n-ik hatványát, ha a kitevő pozitív szám (3 módszer).
+
+hatvanyX :: (Ord a, Floating a) => a -> a -> a
+hatvanyX x n
+  |n<0 =error "neg. kitevo"
+  |otherwise = x**n
+
+
+hatvanyX2 x n 
+  |n<0 = error "neg. kitevo"
+  |otherwise = x^n
+
+hatvanyX3 x n
+  |n<0 = error "neg kitevo"
+  |otherwise = x * hatvanyX3 x (n-1)
+
+
+
+
+
+
 
 -- II. Könyvtárfüggvények használata nélkül, illetve halmazkifejezéseket alkalmazva, definiáljuk azt a függvényt, amely meghatározza:
 
 -- - az első n természetes szám negyzetgyökét,
+negyzetgyok n = [sqrt i | i <-[1 .. n]]
+
+
+
 -- - az első n négyzetszámot,
+
+negyzetszam n= [i ^2 | i<- [1 .. n]]
+
 -- - az első n természetes szám köbét,
+
+kobszam n =[i^3 | i<- [1 .. n]]
+
 -- - az első n olyan természetes számot, amelyben nem szerepelnek a négyzetszámok,
+
+nemNegyzet n= [i| i<- [1 ..n], i /= (sqrt i^2)]
+
 -- - x hatványait adott n-ig,
+
+hatvany x n = [x ^i | i<- [1 .. n ]]
+
 -- - egy szám páros osztóinak listáját,
+
+parosOsztok x = [ i | i <- [1 .. x], mod x i ==0, mod i 2 == 0 ]
+
+parosOsztok2 x = [ i | i <- [1 .. x], mod x i ==0, even i ]
+
+
 -- - n-ig a prímszámok listáját,
+
+osztok x = [i | i<-[1 ..x ], mod x i ==0]
+
+primszam x = osztok x == [1,x]
+
+primszamN n = [ i | i <-[1..n ], primszam i ]
+
+primszamN2 n  = [i | i<-[1 ..n ], primszamL i]
+  where 
+    primszamL sn= [1,sn] == osztokL sn
+    osztokL sn2 = [j| j<- [1..sn2], mod sn2 j ==0]
+
+
+
+
+
 -- - n-ig az összetett számok listáját,
+osszetett n =[i| i<-[1..n], primszam i== False]
+
+osszetett2 n=[i | i<-[1..n], not (primszam i)]
+
+
+
+
 -- - n-ig a páratlan összetett számok listáját,
+
+paratlanOsszetett n=[i | i<-[1..n], not (primszam i), mod i 2 /=0]
+
+paratlanOsszetett2 n=[i | i<-[1..n], not (primszam i), mod i 2 /=0, not (even i)]
+
+paratlanOsszetett3 n=[i | i<-[1..n], not (primszam i), mod i 2 /=0, odd i]
+
+
+
+
 -- - az n-nél kisebb Pitágorászi számhármasokat,
+pitagorasz n = [(a, b, c) | c<-[1..n], b<-[1..c], a<-[1..b], a^2+b^2 ==c^2]
+
+
+
 -- - a következő listát: $$[(\texttt{a},0), (\texttt{b},1),\ldots, (\texttt{z}, 25)]$$,
 -- - a következő listát: $$[(0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0)]$$, majd általánosítsuk a feladatot.
+
+szamok = zip [0..5][5,4 .. 0]
+
+szamok2 n = zip [0..n][n,n-1 ..0]
+
+szamok3 n = [(i,n-1) | i<-[0..n]]
+
 -- - azt a listát, ami felváltva tartalmaz True és False értékeket.
+tf n= take n ls
+  where
+    ls=[True,False] ++ls
+
+tf2 n =[mod i 2 ==0 | i<-[0..n]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- - egyeb
+
+main :: IO ()
+main = do
+  putStrLn "elempar (6,7) (7,6)"
+  print (elempar (6,7) (7,6))
+  let ep1=(6,7)
+  putStrLn("elempar (6,7) (4,7)" ++ show ep1 ++ " "++  show (elempar2 (6,7) (4,7)))
+
+
+  -- -a következő listát: [(a,0),(b,1),…,(z,25)]
+betuSzam = zip ['a' .. 'z'] [0 .. 25]
+
+betuSzam2 = zip ['a'..'z'][1,3..]
---
+++ 02.labor/lab2.hs
@@ -0,0 +1,18 @@
+-- egy szám számjegyeinek szorzatát (2 módszerrel),
+szjSzorat 0 = 1
+
+szjSzorzat n = mod n 10 * szjSzorat (div n 10)
+
+szjSzorat2 n
+  | n < 0 = error "meg.szam"
+  | div n 10 == 0 = n
+  | otherwise = mod n 10 * szjSzorat2 (div n 10)
+
+szjSzorat3 n res
+  | n < 0 = error "neg szam"
+  | div n 10 == 0 = res * n
+  | otherwise = szjSzorat3 (div n 10) (res * (mod n 10))
+
+-- egy szám számjegyeinek összegét (2 módszerrel),
+
+-- egy szám számjegyeinek számát (2 módszerrel),
